09ebded2c1bac00f5d773338563b74414960217a
upkshindwr-stac <upkshindwr1024@gmail.com>
2026-04-27 23:09:21 +0530

    Finished Homework

---
+++ print.py
@@ -0,0 +1,17 @@
+print("PDU Unit, ")
+print("IIT Campus, ")
+print("Colombo.\n")
+
+print("26 April 2026\n")
+
+print("Dear Lucky Sir,\n")
+
+print("I am writing this letter to inform you that i have succesfully completed my first week of learning python.")
+print("During those days, I spent much time to practicing Pyhton programming.")
+print("I have learned basic concepts such as Variables and Simple Programs.\n")
+
+print("Thank you very much sir for your guidance and support throughout this learning process.")
+print("I look forward to continuing my Python Journey.\n")
+
+print("Your Sincerly,")
+print("Upeksha Indeewari")
